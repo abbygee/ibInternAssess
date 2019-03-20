fee7d950082ccafe10b1e160b500dcc3dc293792
abbygee <abigaildelgado@students.berkeley.net>
2019-03-20 16:22:23 -0700

    stuff

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 var check = 0;
 
-
-$.ajax({
-    //this retrieves the supported languages from the server
-    url: "https://translate.yandex.net/api/v1.5/tr.json/getLangs?ui=en&key=trnsl.1.1.20190312T203236Z.0b8cd82697e99ad0.60ad6c537ddb42128b819afad99fecdde65279b8",
-    type: 'POST',
-    success: function(result){
-        console.log(result);
-        createList(result);
-    }
+$(document).ready(function(){
+    $.ajax({
+        //this retrieves the supported languages from the server
+        url: "https://translate.yandex.net/api/v1.5/tr.json/getLangs?ui=en&key=trnsl.1.1.20190312T203236Z.0b8cd82697e99ad0.60ad6c537ddb42128b819afad99fecdde65279b8",
+        type: 'POST',
+        success: function(result){
+            console.log(result);
+            createList(result);
+        }
+    });
+    //makeTwitter()
+    //somehow get it to automatically input MichelleObama as an example
 });
+
 
 //this function serves to create a list of the supported languages for the client to choose from
 function createList(data){
@@ -41,7 +45,6 @@ function checkUser(user){
 function makeTwitter(){
     var user = $("#twtuser").val();
     checkUser(user);
-
     $.ajax({
         url: "https://twitter-relay.herokuapp.com/twitterAPI?user=" + user,
         dataType: 'JSON',
@@ -54,17 +57,21 @@ function makeTwitter(){
             }else{
                 makeProfile(result);
                 makeTweets(result);
+                $(".all-tweets").click(function (){
+                    var tweet = result.statuses[this.id].full_text;
+                    translateTweet(tweet);
+                });
             }
         }
     });
+    $("#profile").css("visibility", "visible");
 }
 
-function translateWord(){
-
+function translateTweet(tweet){
     if(check === 0){
         var lang = $("#langs").val();
         $.ajax({
-            url: "https://translate.yandex.net/api/v1.5/tr.json/translate?text=" + "hello" + "&lang=" + lang + "&key=trnsl.1.1.20190312T203236Z.0b8cd82697e99ad0.60ad6c537ddb42128b819afad99fecdde65279b8",
+            url: "https://translate.yandex.net/api/v1.5/tr.json/translate?lang=" + lang + "&key=trnsl.1.1.20190312T203236Z.0b8cd82697e99ad0.60ad6c537ddb42128b819afad99fecdde65279b8&text=" + tweet,
             type: 'POST',
             success: function(result){
                 var trans = $("#translation");
@@ -85,7 +92,6 @@ function makeProfile(data){
     div.append('<div class="inbl" id="dn">' + data.statuses[0].user.name + '<br><div id="at"> @' + data.statuses[0].user.screen_name + '</div>' + '</div>');
 }
 
-//user, text, entities, ifQuote, quote, date, index
 
 function makeTweets(data){
     var div = $("#tweets");
@@ -96,12 +102,12 @@ function makeTweets(data){
 
 
         if(data.statuses[i].is_quote_status === true){
-            div.append('<div class="all-tweets" id="' + i + '"><div id="quote">@' + data.statuses[i].quoted_status.user.screen_name
+            div.append('<div class="all-tweets" id="' + i + '"><div class="quote">@' + data.statuses[i].quoted_status.user.screen_name
                 + ': "' + data.statuses[i].quoted_status.full_text + '"</div><p>' + data.statuses[i].full_text + '</p>'
                 + date +'</div>');
         }else{
             if(data.statuses[i].entities.user_mentions.length > 0){
-                div.append('<div class="all-tweets" id="' + i + '"><div id="reply">in reply to @' +
+                div.append('<div class="all-tweets" id="' + i + '"><div class="reply">in reply to @' +
                     data.statuses[i].entities.user_mentions[0].screen_name + '</div><p>' + data.statuses[i].full_text + '</p>'
                     + date +'</div>');
 
@@ -112,3 +118,4 @@ function makeTweets(data){
         }
     }
 }
+
