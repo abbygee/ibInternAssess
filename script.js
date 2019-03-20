d8548747e03c8a7e141542bff3d1e5684bdef0fd
abbygee <abigaildelgado@students.berkeley.net>
2019-03-19 21:33:04 -0700

    did lots

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var check = 0;
+
+
 $.ajax({
     //this retrieves the supported languages from the server
     url: "https://translate.yandex.net/api/v1.5/tr.json/getLangs?ui=en&key=trnsl.1.1.20190312T203236Z.0b8cd82697e99ad0.60ad6c537ddb42128b819afad99fecdde65279b8",
@@ -18,62 +21,47 @@ function createList(data){
     }
 }
 
-// function checkUser(){
-//     var user = $("#twtuser").val();
-//     //checks if a user was entered
-//     if(user.length < 1){
-//         alert('Please type in a twitter username');
-//         return false;
-//     }
-//
-//     //check for @ symbol or blank spaces which is not necessary
-//     for(var i = 0; i < user.length; i++){
-//         if(user[i] === "@"){
-//             alert('Please type in the username without any symbols');
-//             return false;
-//         }
-//         if(user[i] === " "){
-//             alert('Make sure to enter the username without spaces');
-//             return false;
-//         }
-//     }
-//
-//     //check if entered username exists
-//     $.ajax({
-//         url: "https://twitter-relay.herokuapp.com/twitterAPI?user=" + user,
-//         dataType: 'JSON',
-//         success: function(result){
-//             console.log(result);
-//             //checkReal(result);
-//         }
-//     });
-// }
+function checkUser(user){
+    //checks if a user was entered
+    if(user.length < 1){
+        alert('Please type in a twitter username');
+    }
 
+    //check for @ symbol or blank spaces which is not necessary
+    for(var i = 0; i < user.length; i++){
+        if(user[i] === "@"){
+            alert('Please type in the username without any symbols');
+        }
+        if(user[i] === " "){
+            alert('Make sure to enter the username without spaces');
+        }
+    }
+}
 
-// function checkReal(data){
-//     if(data.statuses.length === 0) {
-//         alert('Please enter a real existing twitter username');
-//         return false;
-//     }
-// }
-
-
-function translateWord(){
+function makeTwitter(){
     var user = $("#twtuser").val();
+    checkUser(user);
+
     $.ajax({
         url: "https://twitter-relay.herokuapp.com/twitterAPI?user=" + user,
         dataType: 'JSON',
-        success: function (result) {
+        success: function(result){
             console.log(result);
+            if(result.statuses.length === 0) {
+                check++;
+                alert("Twitter username doesn't exist, user is private, or user has no recent activity. Please " +
+                    "try with a different user");
+            }else{
+                makeProfile(result);
+                makeTweets(result);
+            }
         }
     });
+}
 
-    //if((checkUser() !== false) && (checkReal() !== false)){
-        var container = $("#reset");
-        container.empty();
-        container.append('<a id=\"timeline\" class=\"twitter-timeline\" data-width=\"600\" data-height=\"660\" data-link-color=\"#2B7BB9\" href=\"https://twitter.com/' + user + '"></a><script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>');
+function translateWord(){
 
-
+    if(check === 0){
         var lang = $("#langs").val();
         $.ajax({
             url: "https://translate.yandex.net/api/v1.5/tr.json/translate?text=" + "hello" + "&lang=" + lang + "&key=trnsl.1.1.20190312T203236Z.0b8cd82697e99ad0.60ad6c537ddb42128b819afad99fecdde65279b8",
@@ -87,5 +75,40 @@ function translateWord(){
                 alert('Please choose a language for translation!');
             }
         });
-    //}
+    }
+}
+
+function makeProfile(data){
+    var div = $("#profile");
+    div.empty();
+    div.append('<img class="inbl" id="pfp" src="' + data.statuses[0].user.profile_image_url + '">');
+    div.append('<div class="inbl" id="dn">' + data.statuses[0].user.name + '<br><div id="at"> @' + data.statuses[0].user.screen_name + '</div>' + '</div>');
+}
+
+//user, text, entities, ifQuote, quote, date, index
+
+function makeTweets(data){
+    var div = $("#tweets");
+    div.empty();
+
+    for(var i = 0; i < data.statuses.length; i++){
+        var date = data.statuses[i].created_at.substring(0,9);
+
+
+        if(data.statuses[i].is_quote_status === true){
+            div.append('<div class="all-tweets" id="' + i + '"><div id="quote">@' + data.statuses[i].quoted_status.user.screen_name
+                + ': "' + data.statuses[i].quoted_status.full_text + '"</div><p>' + data.statuses[i].full_text + '</p>'
+                + date +'</div>');
+        }else{
+            if(data.statuses[i].entities.user_mentions.length > 0){
+                div.append('<div class="all-tweets" id="' + i + '"><div id="reply">in reply to @' +
+                    data.statuses[i].entities.user_mentions[0].screen_name + '</div><p>' + data.statuses[i].full_text + '</p>'
+                    + date +'</div>');
+
+            }else{
+                div.append('<div class="all-tweets" id="'+ i + '"><p>' + data.statuses[i].full_text + '</p>'
+                    + date + '</div>')
+            }
+        }
+    }
 }
